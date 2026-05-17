4cf1cc827bdf08388eef81276ed1ffd742824a45
Kauan Henry <henry8kauan@gmail.com>
2026-05-17 15:29:58 -0300

    Atualizações

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: false
+});
+
+sr.reveal('.hero-content', { delay: 200 });
+sr.reveal('.secao-info', { delay: 300 });
+sr.reveal('.secao-contato', { delay: 400 });
